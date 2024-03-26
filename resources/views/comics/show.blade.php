c64fec6bc1e81f8560f9e31fe79ebabfe4972557
SerenaMalusa <sere.malu@gmail.com>
2024-03-26 18:20:49 +0100

    feat: add button to go back to comics.index from comics.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,7 +5,12 @@
 @section('main-content')
     <section>
         <div class="container py-4">
-            <h1 class="mb-3">{{ $comic->title }}</h1>
+            <div class="row justify-content-between align-items-center">
+                <h1 class="mb-3 col-6">{{ $comic->title }}</h1>
+                <div class="col-2 text-end">
+                    <a class="btn btn-primary" href="{{ route('comics.index')}}">Back to comics'list</a>
+                </div>
+            </div>
 
             <div class="row">
                 <div class="col-4">
