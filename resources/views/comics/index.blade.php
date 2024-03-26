fa4c17259754e4102c8078c5b183cdcc782ea851
SerenaMalusa <sere.malu@gmail.com>
2024-03-26 17:57:20 +0100

    feat: crete link to each comic detail page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,7 +5,7 @@
 @section('main-content')
   <section>
     <div class="container py-4">
-      <h1 class="mb-5">Comimc's list</h1>
+      <h1 class="mb-5">Comic's list</h1>
 
       <table class="table">
         <thead>
@@ -15,6 +15,7 @@
             <th scope="col">Series</th>
             <th scope="col">Type</th>
             <th scope="col">Price</th>
+            <th class="text-center" scope="col">Links</th>
           </tr>
         </thead>
         <tbody>
@@ -25,6 +26,11 @@
                     <td>{{ $comic->series }}</td>
                     <td>{{ $comic->type }}</td>
                     <td>{{ $comic->price }}</td>
+                    <td class="text-center">
+                        <a href="{{ route('comics.show', $comic) }}">
+                            <i class="fa-solid fa-circle-info"></i>
+                        </a>
+                    </td>
                 </tr>
             @endforeach
           
@@ -34,4 +40,8 @@
       {{-- {{ $comics->links() }} --}}
     </div>
   </section>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
